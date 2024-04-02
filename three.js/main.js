8485b7c5592006a562829bf0b66a24f8e6e8514e
lasha-codes <lashagiorgi420@gmail.com>
2024-04-02 04:53:26 -0700

    learning three.js and 3d is rendering lets goo

---
+++ three.js/main.js
@@ -0,0 +1,28 @@
+import * as THREE from 'three'
+
+// Scene
+const scene = new THREE.Scene()
+
+// Create our sphere
+const geometry = new THREE.SphereGeometry(3, 64, 64)
+const material = new THREE.MeshStandardMaterial({
+  color: '#00FF83',
+})
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+// light
+const light = new THREE.PointLight(0xffff, 40, 100)
+light.position.set(0, 10, 10)
+scene.add(light)
+
+// Camera
+const camera = new THREE.PerspectiveCamera(45, 800 / 600)
+camera.position.z = 20
+scene.add(camera)
+
+// Renderer
+const canvas = document.querySelector('.webgl')
+const renderer = new THREE.WebGLRenderer({ canvas })
+renderer.setSize(800, 600)
+renderer.render(scene, camera)
